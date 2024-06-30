f8344ab9ff2c1a2bf0978dce52a2d936813f3509
Shivam Kumar <life.shivam2394@gmail.com>
2024-07-01 00:32:43 +0530

    GraphQl added and mutations are left

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,32 @@
 import express from "express";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from '@apollo/server/express4'
 
-const app = express();
-const PORT = Number(process.env.PORT) || 8000;
+async function init() {
+    const app = express();
+    const PORT = Number(process.env.PORT) || 8000;
+    app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Server is up and Running'
+    // GraphQL Server
+    const gqlServer = new ApolloServer({
+        typeDefs: ``,      // Schema
+        resolvers: {},    // actual Function/ Buisness Logic
     });
-});
 
-app.listen(PORT, () => {
-    console.log(`Server started on PORT: ${PORT}`);
-});
+    // Starting GQL Server
+    await gqlServer.start();
+
+    app.get('/', (req, res) => {
+        res.json({
+            message: 'Server is up and Running'
+        });
+    });
+
+    app.use('/graphql', expressMiddleware(gqlServer));
+
+    app.listen(PORT, () => {
+        console.log(`Server started on PORT: ${PORT}`);
+    });
+}
+
+init();
